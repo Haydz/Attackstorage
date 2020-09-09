c2938774d55bcd0be325f0f49e83b35fbfc1f024
Haydz <haydnit@gmail.com>
2020-09-08 21:30:05 -0400

    Create loop_installutil.cs

---
+++ loop_installutil.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+ 
+namespace CreateTextFile
+{
+
+class TextFileWriter
+    {
+        static void Main(string[] args)
+        {
+			 Console.WriteLine("Hello World!");  
+  using System;
+using System.IO;
+ 
+namespace CreateTextFile
+{
+
+class TextFileWriter
+    {
+        static void Main(string[] args)
+        {
+			 Console.WriteLine("Hello World!");  
+			 for (int i = 0; i < 5; i++) 
+{
+  Console.WriteLine(i);
+}
+
+           
+        }
+    }
+}
+        }
+    }
+}
