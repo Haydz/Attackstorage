565415c9229da205299a61403a04f4001461049c
Haydz <haydnit@gmail.com>
2020-09-08 21:43:40 -0400

    Update loop_installutil.cs

--- loop_installutil.cs
+++ loop_installutil.cs
@@ -9,26 +9,10 @@ class TextFileWriter
         static void Main(string[] args)
         {
 			 Console.WriteLine("Hello World!");  
-  using System;
-using System.IO;
- 
-namespace CreateTextFile
-{
-
-class TextFileWriter
-    {
-        static void Main(string[] args)
-        {
-			 Console.WriteLine("Hello World!");  
-			 for (int i = 0; i < 5; i++) 
-{
-  Console.WriteLine(i);
-}
-
-           
-        }
-    }
-}
+ for (int i = 0; i < 5; i++) 
+      {
+        Console.WriteLine(i);
+      }  
         }
     }
 }
